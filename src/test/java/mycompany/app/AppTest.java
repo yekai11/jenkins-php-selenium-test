efa16d19d2a3ed5912aaf83ae12f3156e4e7e807
Ye Kai Phang <38521895+yekai11@users.noreply.github.com>
2023-11-08 23:23:02 +0800

    update

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -22,7 +22,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class AppTest {
 	WebDriver driver;
 	WebDriverWait wait;
-	String url = "http://localhost:5000";
+	String url = "http://localhost";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
